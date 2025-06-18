0b1fe0cc069a757a89da7c9ce968aed3337422db
rjrodriguezr <rjrodriguezr@gmail.com>
2025-06-18 13:11:08 -0500

    middlewares/index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,6 @@
-const { authClient } = require('./authClient');
-const { errorHandler } = require('./errorHandler');
-const { modelAuditPlugin } = require('./modelAuditPlugin');
+const authClient = require('./authClient');
+const errorHandler = require('./errorHandler');
+const modelAuditPlugin = require('./modelAuditPlugin');
 
 module.exports = {
     authClient,
