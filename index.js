232463d691f7f829a087538432c4f1d6c1941713
rjrodriguezr <rjrodriguezr@gmail.com>
2025-08-14 21:42:01 -0500

    index.js

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ try {
 } catch (error) {
   console.error('Error leyendo package.json:', error.message);
 }
+
+// Libs - Core, standalone utilities
 const axiosClient = require('./lib/axiosClient');
 const catalog = require('./lib/catalog');
 const constants = require('./lib/constants');
-const { BaseController } = require('./src/controllers');
-const { companyService } = require('./src/services');
-const { Company, User, Customer, Message } = require('./src/models');
 const crypt = require('./lib/crypt');
 const logger = require('./lib/logger');
-const { authClient, middlewareHandleError, modelAuditPlugin, responseHandleError } = require('./src/middlewares');
 const mongoDBService = require('./lib/mongoDBService');
 const redisService = require('./lib/redisService');
-const { buildDeleteValidator, buildGetValidator, buildSaveValidator, buildUpdateValidator,
-  fieldsValidator, validateDate, validateEmail, validateMongoId, validateNumber, validateText } = require('./src/validators');
+
+// Source Modules - Application logic grouped by domain
+// This assumes each directory has an index.js file that exports its public members.
+const controllers = require('./src/controllers');
+const middlewares = require('./src/middlewares');
+const models = require('./src/models');
+const services = require('./src/services');
+const validators = require('./src/validators');
 
 module.exports = {
+  // Libs
   axiosClient,
   catalog,
   constants,
-  companyService,
-  BaseController,
-  Company, User, Customer, Message,
   crypt,
   logger,
-  authClient, middlewareHandleError, modelAuditPlugin, responseHandleError,
   mongoDBService,
   redisService,
-  buildDeleteValidator, buildGetValidator, buildSaveValidator, buildUpdateValidator,
-  fieldsValidator, validateDate, validateEmail, validateMongoId, validateNumber, validateText,
+
+  // Namespaced modules
+  controllers,
+  middlewares,
+  models,
+  services,
+  validators,
 };
